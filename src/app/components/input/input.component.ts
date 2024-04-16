557ec10e099f1074a2e79d59fd75155eaa86d501
FrnBrz <frnbrz@gmail.com>
2024-04-16 13:01:24 +0200

    Add new button-outline and input components

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule } from '@angular/common'
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
 
 @Component({
   selector: 'app-input',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.scss']
+  styleUrls: ['./input.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class InputComponent {
-
+  @Input() id = ''
+  @Input() css: InputType['css'] = 'input-primary'
+  @Input() placeholder?: string | undefined
+  @Input() value?: string | number | undefined
+  @Input() customErrorMessages: Record<string, string> = {}
+  @Input() name!: string
 }
