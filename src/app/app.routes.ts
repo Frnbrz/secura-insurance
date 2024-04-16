3d4a3d81a21cc94a64763f50dfbfbfbaa3437a7f
FrnBrz <frnbrz@gmail.com>
2024-04-16 18:17:19 +0200

    Add new routes to app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
+import { HomePageComponent } from './pages/home-page/home-page.component'
+import { RenovacionesPageDetailsComponent } from './pages/renovaciones-page-details/renovaciones-page-details.component'
+import { RenovacionesPageComponent } from './pages/renovaciones-page/renovaciones-page.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'renovaciones', component: RenovacionesPageComponent },
+  { path: 'renovaciones/:id', component: RenovacionesPageDetailsComponent },
+]
