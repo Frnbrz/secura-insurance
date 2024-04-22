d0feba20e5b260c2607968841a57bd8ec2bdee0a
FrnBrz <frnbrz@gmail.com>
2024-04-22 14:39:28 +0200

    Update profile menu component with new items and styles

--- src/app/components/navbar/components/profile-menu/profile-menu.component.ts
+++ src/app/components/navbar/components/profile-menu/profile-menu.component.ts
@@ -1,12 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core'
 import { MatButtonModule } from '@angular/material/button'
+import { MatDividerModule } from '@angular/material/divider'
 import { MatIconModule } from '@angular/material/icon'
 import { MatMenuModule } from '@angular/material/menu'
 
 @Component({
   selector: 'app-profile-menu',
   standalone: true,
-  imports: [MatButtonModule, MatMenuModule, MatIconModule],
+  imports: [MatButtonModule, MatMenuModule, MatIconModule, MatDividerModule],
   templateUrl: './profile-menu.component.html',
   styleUrls: ['./profile-menu.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
