9ee25b151bf87a0e16225a608f9711ddcd213ab2
FrnBrz <frnbrz@gmail.com>
2024-04-22 14:39:20 +0200

    Update language menu component styles and translations

--- src/app/components/navbar/components/language-menu/language-menu.component.ts
+++ src/app/components/navbar/components/language-menu/language-menu.component.ts
@@ -1,14 +1,25 @@
-import { NgClass, NgFor } from '@angular/common'
+import { NgClass, NgFor, NgIf } from '@angular/common'
 import { ChangeDetectionStrategy, Component } from '@angular/core'
 import { MatButtonModule } from '@angular/material/button'
+import { MatDividerModule } from '@angular/material/divider'
 import { MatIconModule } from '@angular/material/icon'
 import { MatMenuModule } from '@angular/material/menu'
+import { TranslateModule, TranslateService } from '@ngx-translate/core'
 import { LANGUAGES, LanguageInterface } from '@src/app/components/navbar'
 
 @Component({
   selector: 'app-language-menu',
   standalone: true,
-  imports: [MatButtonModule, MatMenuModule, MatIconModule, NgFor, NgClass],
+  imports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
+    NgFor,
+    NgClass,
+    NgIf,
+    TranslateModule,
+    MatDividerModule,
+  ],
   templateUrl: './language-menu.component.html',
   styleUrls: ['./language-menu.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -20,8 +31,17 @@ export class LanguageMenuComponent {
       lang => lang.id === window.localStorage.getItem('lang')
     ) || this.languages[0]
 
+  constructor(public translate: TranslateService) {
+    translate.addLangs(['en', 'es'])
+    translate.setDefaultLang('es')
+
+    const browserLang = translate.getBrowserLang() || 'es'
+    translate.use(browserLang.match(/en|es/) ? browserLang : 'es')
+  }
+
   changeLanguage(lang: LanguageInterface) {
     this.selectedLanguage = lang
+    this.translate.use(lang.id)
     window.localStorage.setItem('lang', lang.id)
   }
 }
