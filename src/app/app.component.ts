26335d0a8629b07702f4b2b6644e40da368be335
FrnBrz <frnbrz@gmail.com>
2024-04-22 14:38:50 +0200

    Update i18n translations for English and Spanish, add new font icons, and update imports and styles in various components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
+import { NgFor } from '@angular/common'
+import { HttpClient } from '@angular/common/http'
 import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'
+import 'zone.js'
 import { FooterComponent, NavbarComponent } from './components'
+
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient)
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, NgFor],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'secura-insurance'
-}
+export class AppComponent {}
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,25 @@
-import { ApplicationConfig } from '@angular/core'
+import { ApplicationConfig, importProvidersFrom } from '@angular/core'
 import { provideRouter } from '@angular/router'
 
+import { HttpClient, provideHttpClient } from '@angular/common/http'
 import { provideAnimations } from '@angular/platform-browser/animations'
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core'
+import { HttpLoaderFactory } from './app.component'
 import { routes } from './app.routes'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations()],
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    provideHttpClient(),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient],
+        },
+      })
+    ),
+  ],
 }
